cf9025f9981d5db10dc1f5a3247611de066885c8
Dat Nguyen <ndatyu11@gmail.com>
2025-09-29 23:06:36 +0700

    Update template login, register

---
+++ client/src/features/account/login/login.ts
@@ -0,0 +1,37 @@
+import { Component, inject, output, signal } from '@angular/core';
+import { AccountService } from '../../../core/service/account-service';
+import { LoginCreds } from '../../../_models/user';
+import { FormsModule } from '@angular/forms';
+import { Register } from "../register/register.component";
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, Register],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  
+  accountService = inject(AccountService);
+
+    protected creds: any = {} as LoginCreds;
+    protected registerMode = signal(false);
+  
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: result => {
+        console.log(result),
+        this.creds ={};
+      },
+      error: error => alert(error.error)
+    })
+
+  }
+
+   showRegister(value: boolean) {
+    this.registerMode.set(value);
+  }
+
+
+
+}
