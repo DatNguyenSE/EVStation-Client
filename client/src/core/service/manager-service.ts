479e7789b95f4a5b4f004e366fd709d590ab16e5
nguyen <nguyendeptrai23092005@gmail.com>
2025-11-11 06:54:51 +0700

    feat(manager-home): get assginment by admin

--- client/src/core/service/manager-service.ts
+++ client/src/core/service/manager-service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { AssignmentResponseDto } from '../../_models/user';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
+
+   private http = inject(HttpClient);
+    baseUrl = 'https://localhost:5001/api/';
+
+    getAssignment(idStaff: string) {
+        return this.http.get<AssignmentResponseDto>(`${this.baseUrl}assignments/staff/${idStaff}`)
+    }
+
+    
+
+
   
 }
